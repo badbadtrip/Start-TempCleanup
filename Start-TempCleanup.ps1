26970affe9202a7891283f034828b2da627708f0
Egor Naidovich <e.naidovich@gmail.com>
2022-04-08 10:38:36 +0300

    Merge branch 'master' of https://github.com/badbadtrip/Start-TempCleanup

--- Start-TempCleanup.ps1
+++ Start-TempCleanup.ps1
@@ -19,21 +19,18 @@
   Version:  1.0.0
   Date:     2022-04-06
   Author:   Egor Naidovich
-  Author:   Viacheslav Eliseev
+  Author:   Viachaslav Eliseev
   Changes:  Initial version
 
 .EXAMPLE
   PS> .\Start-TempCleanup.ps1
 #>
 
-$LogName = 'TempCleanup'
-if ([System.Diagnostics.EventLog]::Exists($LogName -eq $False)) {
-  New-EventLog -LogName 'TempCleanup' -Source 'Start-TempCleanup.ps1'
-}
-
-Clear-EventLog $LogName
-
-Write-EventLog -LogName $LogName -Source 'Start-TempCleanup.ps1' -EntryType 'Information' -EventId 1 -Message 'Cleaning cleanmgr started.'
+$Date = Get-Date -Format  dd-MM-yyyy
+$Logfile = (New-Item -Path "C:\Logs\$Date.log" -force)
+$RecyclePath = '{0}\$Recycle.bin\' -f $env:SystemDrive
+$CleanmgrPath = '{0}\System32\CleanMgr.exe' -f $env:SystemRoot
+$RegeditPath = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches'
 $SettingsList = @(
   'Active Setup Temp Folders',
   'D3D Shader Cache',
@@ -55,24 +52,40 @@ $SettingsList = @(
   'Windows Error Reporting Files'
 )
 
-$RegeditPath = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches'
+function WriteLog {
+  Param ([string]$LogString)
+
+  $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+  $LogMessage = "$Stamp $LogString"
+  Add-content $LogFile -value $LogMessage -Force
+}
+
+WriteLog -LogString 'Script started'
+WriteLog -LogString 'Registry patch launched'
 
 foreach ($s in $SettingsList) {
   $StrPath = '{0}\{1}' -f $RegeditPath, $s
-  Set-ItemProperty -Path $StrPath -Name 'StateFlags0004' -Value 2 -ErrorAction SilentlyContinue
+  if (Test-Path -Path $StrPath) {
+    Set-ItemProperty -Path $StrPath -Name 'StateFlags0004' -Value 2
+  }
 }
 
-$CleanmgrPath = '{0}\System32\CleanMgr.exe' -f $env:SystemRoot
-Start-Process -FilePath $CleanmgrPath -ArgumentList '/sagerun:4' -WindowStyle Hidden -Wait
-Write-EventLog -LogName $LogName -Source 'Start-TempCleanup.ps1' -EntryType 'Information' -EventId 1 -Message 'Cleaning cleanmgr completed.'
+WriteLog -LogString 'Cleanup started CleanMgr'
+Start-Process -FilePath $CleanmgrPath -ArgumentList '/sagerun:4' -WindowStyle Hidden -Wait -ErrorAction SilentlyContinue
+
 
 $Users = Get-ChildItem -Path 'C:\Users'
 
+WriteLog -LogString 'Temp folder cleanup started'
 foreach ($u in $Users) {
   $curTempPath = '{0}\AppData\Local\Temp\*' -f $u.FullName
   if (Test-Path -Path $curTempPath) {
     Remove-Item -Path $curTempPath -Force -Recurse -ErrorAction Ignore
   }
 }
-Write-EventLog -LogName $LogName -Source 'Start-TempCleanup.ps1' -EntryType 'Information' -EventId 1 -Message 'Cleaning Temp completed.'
-Clear-RecycleBin -Force
+
+WriteLog -LogString 'Launched recycle.bin cleanup for all users'
+$RecyclePath = '{0}\$Recycle.bin\' -f $env:SystemDrive
+Get-ChildItem $RecyclePath -Force | Remove-Item -Recurse -Force
+
+WriteLog -LogString 'Script completed successfully'
